c917de549a5ac57b87a5de365e7bd8ec0b238d21
NET\dulguun.d <dulguun.d@mobicom.mn>
2022-04-26 10:45:31 +0800

    test

--- dto/newDataRequest_test.go
+++ dto/newDataRequest_test.go
@@ -5,20 +5,53 @@ import (
 	"testing"
 )
 
-func Test_should_return_error_when_new_data_temperature_is_not_correct(t *testing.T){
+func Test_should_return_error_when_new_data_temperature_is_too_low(t *testing.T) {
 	//Arrange
-	request:= NewDataRequest{
-		Temp: -100,
+	request := NewDataRequest{
+		Temp: -101,
 	}
 
 	//Act
-	appError:= request.Validate()
+	appError := request.Validate()
 
 	//Assert
-	if appError.Message != "Check temperature value"{
+	if appError.Message != "Check temperature value" {
 		t.Error("Invalid message while testing New Data Request")
 	}
-	if appError.Code != http.StatusUnprocessableEntity{
+	if appError.Code != http.StatusUnprocessableEntity {
 		t.Error("Invalid code while testing New Data Request")
+	}
+}
+
+func Test_should_return_error_when_new_data_temperature_is_too_high(t *testing.T) {
+	//Arrange
+	request := NewDataRequest{
+		Temp: 101,
+	}
+
+	//Act
+	appError := request.Validate()
+
+	//Assert
+	if appError.Message != "Check temperature value" {
+		t.Error("Invalid message while testing New Data Request")
+	}
+	if appError.Code != http.StatusUnprocessableEntity {
+		t.Error("Invalid code while testing New Data Request")
+	}
+}
+
+func Test_should_return_no_error_when_new_data_temperature_is_ok(t *testing.T) {
+	//Arrange
+	request := NewDataRequest{
+		Temp: 23,
+	}
+
+	//Act
+	appError := request.Validate()
+
+	//Assert
+	if appError != nil {
+		t.Error("Validation function is not working")
 	}
 }
